64322c3b88b6ba9980d456dc2914e2cbdde023a2
shivam427 <shivamupadhayaya11@gmail.com>
2021-03-03 11:14:36 +0530

    Function Calculation

---
+++ school.js
@@ -0,0 +1,8 @@
+function calculate(input) {
+    var result = input * input + input
+      return result
+}
+
+console.log(calculate(23, 34))
+console.log(calculate(19, 28))
+console.log(calculate(65, 45))
